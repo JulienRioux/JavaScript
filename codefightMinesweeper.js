4f09138b5db92f2d525eaca616bf66e2bbf9b9c5
Julien Rioux <julien.rioux@hotmail.com>
2018-02-21 20:08:13 -0500

    Update codefightMinesweeper.js

--- codefightMinesweeper.js
+++ codefightMinesweeper.js
@@ -1,3 +1,17 @@
+// Example
+
+// For
+
+// matrix = [[true, false, false],
+//           [false, true, false],
+//           [false, false, false]]
+// the output should be
+
+// minesweeper(matrix) = [[1, 2, 1],
+//                        [2, 1, 1],
+//                        [1, 1, 1]]     
+
+
 function minesweeper(matrix){
     
     let result = [];
